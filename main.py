e2820f4eaa2adc6342ec4f4bf7b3f20c1138a83a
Michael Vogt <mvo@ubuntu.com>
2023-09-10 12:11:47 +0200

    WIP

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import logging
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 import os.path
@@ -19,13 +20,20 @@ def tg_send_photo():
     cfg = load_config()
     tgbot = telegram.bot.Bot(cfg["telegram-bot-token"])
     chat_id = cfg["telegram-chat-id"]
+    caption = cfg.get("intercom-img-caption", "ring, ring")
     #tgbot.send_message(chat_id, "klingeling")
+    # XXX: exceptons are not logged right now
     f = urllib.request.urlopen(cfg["intercom-img-url"])
-    tgbot.send_photo(chat_id, f, caption="klingening")
+    tgbot.send_photo(chat_id, f, caption=caption)
     
     
 class TgDoorBridge(BaseHTTPRequestHandler):
     def do_GET(self):
+        if self.server.trigger_path != "" and self.server.trigger_path != self.path:
+            logging.debug(f"not a intercom-trigger-path: f{self.path}")
+            self.send_response(404)
+            self.end_headers()
+            return
         self.send_response(200)
         self.end_headers()
         tg_send_photo()
@@ -33,8 +41,10 @@ class TgDoorBridge(BaseHTTPRequestHandler):
 
 def main():
     cfg = load_config()
+    #logging.basicConfig(level=logging.DEBUG)
 
     srv = HTTPServer((cfg["hostname"], cfg["port"]), TgDoorBridge)
+    srv.trigger_path = cfg.get("intercom-trigger-path", "")
     print("tg door bridge http://%s:%s" % (cfg["hostname"], cfg["port"]))
 
     try:
